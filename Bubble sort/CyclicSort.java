440e79795ab0ff2df0ca6d94251cae0920bf8e9a
Kshitij kumar singh <66892634+Kshitijkumar15@users.noreply.github.com>
2023-08-24 11:54:19 +0530

    CyclicSort.java

--- Bubble sort/CyclicSort.java
+++ Bubble sort/CyclicSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class CyclicSort {
     public static void main(String[] args) {
-        int[] arr = {5, 4, 3, 2, 1};
+        int[] arr = {5, 4, 3, 2,1};
         sort(arr);
         System.out.println(Arrays.toString(arr));
     }
